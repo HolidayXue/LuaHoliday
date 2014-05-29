64ca40e433ac0a01648e857a4748bf542a0ac6af
HolidayXue <HolidayXue@users.noreply.github.com>
2014-05-29 19:14:17 +0800

    +）添加入可变参数的函数调用方法
    使得c可以调用任意个数参数的lua的函数的通用接口。

--- Lua/LuaScriptVM/LuaRunner.cpp
+++ Lua/LuaScriptVM/LuaRunner.cpp
@@ -206,5 +206,119 @@ int CLuaRunner::CallLuaFunction(const std::string &function_name, const std::str
 	return return_ret;
 }
 
+int CLuaRunner::CallFormatLuaFunction(const std::string &function_name, const char *fmt, ...)
+{
+	int return_ret = 0;
+	do
+	{
+		if (NULL == m_pLuaState)
+		{
+			return_ret = 1;
+			break;
+		}
+		std::vector<std::string> table_vector;
+		table_vector.clear();
+		std::string call_function_name = "";
+		std::string tmp_sub_string = function_name;
+		int idx = -1;
+		while ((idx = tmp_sub_string.find(".")) != tmp_sub_string.npos)
+		{
+			std::string buffer_string = tmp_sub_string;
+			std::string input_table_name = tmp_sub_string.substr(0, idx);
+			tmp_sub_string = buffer_string.substr(idx + 1);
+			table_vector.push_back(input_table_name);
+		}
+		call_function_name = tmp_sub_string;
+		size_t table_vector_size = table_vector.size();
+		if (table_vector_size > 0)
+		{
+			lua_getglobal(m_pLuaState, table_vector[0].c_str());
+			for (size_t i = 1; i < table_vector.size(); i++)
+			{
+				lua_pushstring(m_pLuaState, table_vector[i].c_str());
+				lua_gettable(m_pLuaState, -2);
+			}
+
+			lua_pushstring(m_pLuaState, call_function_name.c_str());
+			lua_gettable(m_pLuaState, -2);
+		}
+		else
+		{
+			lua_getglobal(m_pLuaState, function_name.c_str());
+		}
+
+		if (!lua_isfunction(m_pLuaState, -1))
+		{
+			return_ret = -1;
+			break;
+		}
+		va_list argp;
+		va_start(argp, fmt);
+		int argp_count =  CountFormatString(fmt);
+		lua_pushvfstring(m_pLuaState, fmt, argp);
+		va_end(argp);
+
+		int return_ret = lua_pcall(m_pLuaState, argp_count, LUA_MULTRET, 0);
+		if (return_ret != 0)
+		{
+			break;
+		}
+		return_ret = 0;
+	} while (0);
+	return return_ret;
+}
+
+int CLuaRunner::CountFormatString(const char*fmt)
+{
+	int n = 0;
+	for (;;)
+	{
+		const char *e = strchr(fmt, '%');
+		if (e == NULL)
+		{
+			break;
+		}
+		switch (*(e + 1))
+		{
+			case 's': 
+			{
+						n += 1;
+				break;
+			}
+			case 'c': 
+			{
+						n += 1;
+				break;
+			}
+			case 'd':
+			{
+						n += 1;
+				break;
+			}
+			case 'f': 
+			{
+						n += 1;
+				break;
+			}
+			case 'p': 
+			{
+						n += 1;
+				break;
+			}
+			case '%': 
+			{
+						n += 1;
+				break;
+			}
+			default:
+				{
+					   n += 1;
+				break;
+			}
+		}
+		fmt = e + 2;
+	}
+	return n;
+}
   
 
