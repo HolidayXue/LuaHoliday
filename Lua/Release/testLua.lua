2830c6b5f879c8da7ab38bcc98523afc2005833e
HolidayXue <HolidayXue@users.noreply.github.com>
2014-05-30 11:24:32 +0800

    *）修改调用函数为表里面的函数
    将可以调用的函数改为可以调用表里面的函数

--- Lua/Release/testLua.lua
+++ Lua/Release/testLua.lua
@@ -1,20 +1,36 @@
 --package.cpath = package.cpath .. “.\\?.dll”
+G_TestLuaTable = {} 
+G_TestLuaTable.int_param = 0;
+G_TestLuaTable.test1 = {}
+G_TestLuaTable.test1.test4 = {}
 local json = require ("cjson");
 local s = require "testHoliday";
 print("hello world!");
 G_TestObject1:print("I'm G_TestObject1\n");
 G_TestObject2:print("I'm G_TestObject2\n");
 s.print();
-function testCall(str_input)
+
+function G_TestLuaTable.testCall(str_input)
 	print(str_input);	
 	local table_jsonout = json.decode(str_input);
-	print(table_jsonout["key"])
+	print(table_jsonout["key"]);
 end
-function test_call_first(str_input)
+
+function G_TestLuaTable.test1.test2(str_input)
+	print(str_input);
+	
+end
+
+function G_TestLuaTable.test1.test4.test2(str_input)
+	print(str_input)
+end
+
+function G_TestLuaTable.test_call_first(str_input)
+	G_TestLuaTable.int_param =G_TestLuaTable.int_param-1;
 	print("test_call_first start\n");
 	print(str_input);	
 	local table_json_input = {};
-	table_json_input["function_name"] = "test_call_second";
+	table_json_input["function_name"] = "G_TestLuaTable.test_call_second";
 	table_json_input["param_email"] = "362783516@qq.com";
 	table_json_input["param_name"] = "HolidayXue";	
 	local str_output = json.encode(table_json_input);	
@@ -26,17 +42,38 @@ function test_call_first(str_input)
 	print("test_call_first end\n")
 	
 end 
-function test_call_second(str_input)
-    print("test_call_second start\n")	
-	print(str_input);	
-	local table_jsonout = json.decode(str_input);	
-	print(table_jsonout["param_name"]);
+function G_TestLuaTable.test_call_second(str_input)
+	print("test_call_second start\n")
+	G_TestLuaTable.int_param =G_TestLuaTable.int_param+2;
+	
+	if G_TestLuaTable.int_param<25 then	
+		print(str_input);	
+		local table_jsonout = json.decode(str_input);	
+		print(table_jsonout["param_name"]);
+		local table_json_input = {};
+		table_json_input["function_name"] = "G_TestLuaTable.test_call_first";
+		table_json_input["param_email"] = "362783516@qq.com";
+		table_json_input["param_name"] = "HolidayXue";	
+		local str_output = json.encode(table_json_input);
+		G_AsCInterFace1:test_calledFunction(str_output);
+	else
+		local table_json_input = {};
+		table_json_input["function_name"] = "G_TestLuaTable.test_call_third";
+		table_json_input["param_email"] = "362783516@qq.com";
+		table_json_input["param_name"] = "HolidayXue";	
+		local str_output = json.encode(table_json_input);
+		G_AsCInterFace1:test_calledFunction(str_output);
+	end
+    
+	print("test_call_second end\n")
+end
+
+function G_TestLuaTable.test_call_third(str_input)
+	G_TestLuaTable.int_param =G_TestLuaTable.int_param-10;
 	local table_json_input = {};
-	table_json_input["function_name"] = "test_call_first";
+	table_json_input["function_name"] = "G_TestLuaTable.test_call_second";
 	table_json_input["param_email"] = "362783516@qq.com";
 	table_json_input["param_name"] = "HolidayXue";	
 	local str_output = json.encode(table_json_input);
 	G_AsCInterFace1:test_calledFunction(str_output);
-	
-	print("test_call_second end\n")
 end
